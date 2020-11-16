bd6eb01d561e8557d6ffe16ddc618a54d324de06
Joefive <joefive@yeah.net>
2020-11-16 20:50:45 +0800

    new

--- servlet-04-servletResponse/src/main/java/com/sunrise/FileServlet.java
+++ servlet-04-servletResponse/src/main/java/com/sunrise/FileServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.Set;
 
 public class FileServlet extends HttpServlet {
@@ -16,14 +17,14 @@ public class FileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1.通过上下文获取文件绝对路径
         //String realPath = this.getServletContext().getRealPath("/WEB-INF/classes/1.jpg");
-        String realPath ="D:\\MyWork\\web-servlet\\servlet-04-servletResponse\\target\\classes\\1.png";
+        String realPath ="D:\\MyWork\\web-servlet\\servlet-04-servletResponse\\target\\classes\\图片.png";
         //D:\MyWork\web-servlet\servlet-04-servletResponse\target\classes\1. png
         System.out.println("要下载文件路径：" + realPath);
         //2.获取下载文件名称，截取斜杠以后的
         String fileName = realPath.substring(realPath.lastIndexOf("\\") + 1);
         System.out.println("要下载文件名：" + fileName);
         //3.让浏览器能够支持下载我们需要的东西,WEB下载文件头信息
-        resp.setHeader("Content-disposition","attachment;fileName="+fileName);
+        resp.setHeader("Content-disposition","attachment;fileName="+ URLEncoder.encode(fileName,"utf-8"));
         //4.获取文件下载输入流
         FileInputStream in = new FileInputStream(realPath);
         //5.创建缓冲区
